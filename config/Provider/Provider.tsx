33423ce0036e0fc4ad44dff6f560ed766834c4c2
Being-Murshid <murshid71704@gmail.com>
2023-08-28 06:01:40 -0700

    fix: setup all file eg. herosection , utils , layout , types

---
+++ config/Provider/Provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { ThemeProvider } from "next-themes";
+import type { ChildrenProps } from "../../src/types/types";
+
+function Provider({ children }: ChildrenProps) {
+  const [hydration, setHydration] = React.useState(false);
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHydration(true);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider attribute="class">
+      {hydration ? <>{children}</> : <></>}
+    </ThemeProvider>
+  );
+}
+
+export default Provider;
---
+++ src/components/matarial/HeroSecton/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1> this is new project </h1>
+    </div>
+  )
+}
+
+export default HeroSection
