b5611d3778089e62e18ae1c7d1dca98259541aea
Being-Murshid <murshid71704@gmail.com>
2024-02-26 08:17:40 -0800

    create dumy data with typescript

--- src/components/reuseable/BgLine/BgLine.tsx
+++ src/components/reuseable/BgLine/BgLine.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import {info} from "../../../../types/Info"
 const BgLine = () => {
+  console.log(info , "logs info")
   return <div>ok</div>;
 };
 
---
+++ types/Info.ts
@@ -0,0 +1,18 @@
+// Define a type for the User object
+type User = {
+    name: string;
+    education: string;
+    degree: string;
+};
+
+// Define the array of User objects
+export const info: User[] = [
+    {
+        name: "muhammad saqlain",
+        education: "bs in computer sciences",
+        degree: "software engineering"
+    }
+];
+
+// Export the info array
+export default info;
