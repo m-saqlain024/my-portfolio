7bbf32109a198072af5ca6600a1b181257615446
Being-Murshid <murshid71704@gmail.com>
2024-02-27 06:44:33 -0800

    fix display of the porfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-
-
 export default function Home() {
   return (
-    <>
-    this is  my Porfolio
-    </>
-  )
+    <div className="w-full">
+      <h1>
+        this is a heading Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Illo, ab minima dolorum dolores deserunt facilis dolor dolorem
+        iste temporibus aspernatur libero exercitationem eos cumque cum
+        voluptatibus ratione magni veniam. Corrupti.{" "}
+      </h1>
+    </div>
+  );
 }
