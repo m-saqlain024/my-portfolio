bcc6ca83ab222cfaba03d46063041993cac54958
Being-Murshid <murshid71704@gmail.com>
2024-03-04 05:06:39 -0800

    create a about section

--- src/components/libs/Navigation/Navigtion.tsx
+++ src/components/libs/Navigation/Navigtion.tsx
@@ -1,6 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
-const nav = ["home", "about", "skills", "educations", "blogs"];
+const nav = [
+  {
+    name: "home ",
+    href: "/#home",
+  },
+  {
+    name: "about ",
+    href: "/#about",
+  },
+  {
+    name: "skills ",
+    href: "/#skills",
+  },
+  {
+    name: "educations ",
+    href: "/#educations",
+  },
+  {
+    name: "blogs ",
+    href: "/#blogs",
+  },
+];
+// const nav = ["home", "about", "skills", "educations", "blogs"];
 
 function Navigtion() {
   return (
@@ -11,7 +34,7 @@ function Navigtion() {
             className="text-center hover:text-blue-500 hover:underline leading-6 text-base py-1.5"
             key={index}
           >
-            {items}
+            <Link href={items.href}>{items.name}</Link>
           </li>
         );
       })}
